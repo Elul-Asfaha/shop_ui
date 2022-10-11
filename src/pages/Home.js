da483aaae4aa786f8e5686a24096f6c8c86130b3
Elul-Asfaha <69459338+Elul-Asfaha@users.noreply.github.com>
2022-10-11 20:43:02 +0300

    Add files via upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,15 @@ import styled from 'styled-components'
 import Announcment from '../components/Announcment.js'
 import Catagories from '../components/Catagories.js'
 import Navbar from '../components/Navbar.js'
+import Newsletter from '../components/Newsletter.js'
+import Products from '../components/Products.js'
 import Slider from '../components/Slider.js'
 
 
 const Container=styled.div`
-width: 100vw;`
+width: 100vw;
+min-width: 337px;
+`
 const Home=()=>{
 return(
     <Container>
@@ -14,6 +18,8 @@ return(
         <Navbar/>
         <Slider/>
         <Catagories/>
+        <Products/>
+        <Newsletter/>
     </Container>
 )
 }
